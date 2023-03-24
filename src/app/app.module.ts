636a24f1a2c06f4475bee318ab499712cab759fa
3sspace <vinudbe@ukr.net>
2023-03-24 15:14:16 +0200

    projectv2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { PostComponent } from './post/post.component';
+import { UserComponent } from './user/user.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent,
-    PostComponent
+    UserComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule
